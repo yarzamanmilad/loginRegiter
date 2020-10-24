b1e1ed5260c96cbe77378a7eb03daf11412155b0
yarzamanmilad <my.1370.6870@gmail.com>
2020-10-24 22:23:54 +0330

    preapare project.

---
+++ vivification/src/main/java/com/labaik/vivification/VivificationApplication.java
@@ -0,0 +1,13 @@
+package com.labaik.vivification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VivificationApplication.class, args);
+    }
+
+}
---
+++ vivification/src/test/java/com/labaik/vivification/VivificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labaik.vivification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
